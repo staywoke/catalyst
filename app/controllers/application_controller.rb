72b8b2ef9db544163c0f2099d41ec0d5e8273693
C.J. Neff <github@cjneff.com>
2016-10-03 12:26:54 -0700

    Bake out Devise controllers and views
    #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for
+    dashboard_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(
+        :first_name,
+        :last_name,
+        :legacy_survey_response_id,
+        :zip_code,
+      )
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(
+        :first_name,
+        :last_name,
+        :zip_code,
+      )
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  devise_for :users
+  devise_for :users, controller: {sessions: 'users/sessions'}
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
 
   get 'welcome', to: 'welcome#new', as: :welcome
   get 'welcome/:token', to: 'welcome#new'
