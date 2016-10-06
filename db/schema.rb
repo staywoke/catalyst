b6ff4f81a7e0b94469e7279390bd04a9fa4daa9c
C.J. Neff <github@cjneff.com>
2016-10-05 23:15:40 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006051011) do
+ActiveRecord::Schema.define(version: 20161006051742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20161006051011) do
     t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "domain_memberships", force: :cascade do |t|
+    t.integer  "domain_id"
+    t.integer  "city_id"
+    t.integer  "county_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_domain_memberships_on_city_id", using: :btree
+    t.index ["county_id"], name: "index_domain_memberships_on_county_id", using: :btree
+    t.index ["domain_id"], name: "index_domain_memberships_on_domain_id", using: :btree
   end
 
   create_table "domains", force: :cascade do |t|
@@ -70,4 +81,7 @@ ActiveRecord::Schema.define(version: 20161006051011) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "domain_memberships", "cities"
+  add_foreign_key "domain_memberships", "counties"
+  add_foreign_key "domain_memberships", "domains"
 end
