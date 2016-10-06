bca6077f6a7838c1c45bcf22182331e8a0f08e9b
C.J. Neff <github@cjneff.com>
2016-10-06 16:30:07 -0400

    bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   rescue_from 'NotAuthorized' do |exception|
     render_error_page(status: 403, text: 'Forbidden')
   end
