7f3d49e35f9006d8992274cc7b69d24b28e07754
C.J. Neff <github@cjneff.com>
2016-10-11 19:55:32 -0400

    whitespace

--- app/models/projects/use_of_force_policy.rb
+++ app/models/projects/use_of_force_policy.rb
@@ -3,6 +3,7 @@ class Projects::UseOfForcePolicy < Projects::Base
 
   KEY = 'use_of_force_policy'
   NAME = 'Use Of Force Policy'
+
   BLURB = 'projects/use_of_force_policy/blurb'
   DESCRIPTION = 'projects/use_of_force_policy/description'
 end
