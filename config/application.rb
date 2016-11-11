91256ae6a5e68c287a1aa539ed9090ab3b7c32c6
Ryan Taylor Long <ryan@rtlong.com>
2016-11-11 16:20:21 -0500

    Configure logging a bit

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,24 @@ module Catalyst
     # -- all .rb files in that directory are automatically loaded.
     config.external_url = URI(ENV.fetch('EXTERNAL_URL', 'http://localhost:3000'))
     config.running_in_docker = File.exist?('/.dockerenv')
+
+    config.log_level = ENV.fetch('LOG_LEVEL', 'info')
+
+    # Prepend all log lines with the a per-request UUID (this helps tie multi-line log events together with each other and with events from a downstream Nginx, for example).
+    config.log_tags = [:uuid]
+
+    # Use a log formatter which both includes progname and correctly tags multi-line log events
+    config.log_formatter = -> (severity, time, progname, msg) do
+      line_prefix = severity[0, 1] << ' '
+      line_prefix << "[#{progname}] " if progname.present?
+      first_line, *lines = msg.split("\n")
+      if (m = /\A(?:\[.+?\] )+/.match(first_line))
+        line_prefix << m.to_s
+        first_line = m.post_match
+      end
+      lines.inject(line_prefix + first_line << "\n") do |string, line|
+        string << line_prefix << line << "\n"
+      end
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.logger = ActiveSupport::TaggedLogging.new(
+    ActiveSupport::Logger.new(STDOUT).tap do |logger|
+      logger.formatter = config.log_formatter
+    end
+  )
 end
