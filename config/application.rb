71229951e29991e08a21fea509716e29343f3189
C.J. Neff <github@cjneff.com>
2016-11-11 12:02:40 -0800

    Revert to simple styling on Sign In page

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Catalyst
   class Application < Rails::Application
-    config.to_prepare do
-      Devise::SessionsController.layout "devise_layout"
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
